7d154ebc25346277d6d57ac16cba28d732ee9891
Derrick <1174308565@qq.com>
2018-08-15 23:20:49 +0800

    2018-08-15 合作伙伴图片实现圆角化

--- app/src/main/java/com/example/derrick/zeropoint/adapter/RecyclerViewPartnerAdapter.java
+++ app/src/main/java/com/example/derrick/zeropoint/adapter/RecyclerViewPartnerAdapter.java
@@ -2,6 +2,7 @@ package com.example.derrick.zeropoint.adapter;
 
 import android.content.Context;
 import android.media.Image;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.derrick.zeropoint.R;
 import com.example.derrick.zeropoint.gson.MainDatPartner;
+import com.example.derrick.zeropoint.layoutManage.GlideRoundTransform;
 import com.example.derrick.zeropoint.util.HttpUtil;
 
 import java.util.List;
@@ -50,10 +52,11 @@ public class RecyclerViewPartnerAdapter extends RecyclerView.Adapter<RecyclerVie
             holder.textViewIntro.setText(dat.businesser_introduce);
         }
         if(dat.businesser_photo == null){
-            Glide.with(context).load("http://lf.upingou.com/upload/admin/20180330/0fb8fbbbef99d82fad86cbe562ad4ebb.png").into(holder.imageView);
+            Glide.with(context).load(R.mipmap.ic_launcher).bitmapTransform(new GlideRoundTransform(context)).into(holder.imageView);
+//            holder.imageView.setImageURI(Uri.parse(R.mipmap.ic_launcher));
         }else{
             String uri = HttpUtil.httpFetchCommonUrl+dat.businesser_photo.get(0);
-            Glide.with(context).load(uri).override(60,60).into(holder.imageView);
+            Glide.with(context).load(uri).bitmapTransform(new GlideRoundTransform(context)).into(holder.imageView);
         }
 
 
