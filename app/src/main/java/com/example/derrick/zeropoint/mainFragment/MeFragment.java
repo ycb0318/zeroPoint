2dcc5dbcd112adc984047a15de878ee1d32e1f32
Derrick <1174308565@qq.com>
2018-09-05 16:48:59 +0800

    钱包页面布局完成

--- app/src/main/java/com/example/derrick/zeropoint/mainFragment/MeFragment.java
+++ app/src/main/java/com/example/derrick/zeropoint/mainFragment/MeFragment.java
@@ -14,6 +14,7 @@ import com.example.derrick.zeropoint.R;
  */
 
 public class MeFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/example/derrick/zeropoint/mainFragment/WalletFragment.java
+++ app/src/main/java/com/example/derrick/zeropoint/mainFragment/WalletFragment.java
@@ -1,12 +1,16 @@
 package com.example.derrick.zeropoint.mainFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.derrick.zeropoint.LoginActivity;
 import com.example.derrick.zeropoint.R;
 
 /**
@@ -14,14 +18,38 @@ import com.example.derrick.zeropoint.R;
  */
 
 public class WalletFragment extends Fragment {
+
+    private static final String TAG = "WalletFragment";
+
+    private View square;
+    private LinearLayout scanLinearLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.wallet_fragment,container,false);
+         square = inflater.inflate(R.layout.wallet_fragment,container,false);
+         initView();
+         return square;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initClick();
+
+    }
+
+    private void initView(){
+        scanLinearLayout = (LinearLayout) square.findViewById(R.id.wallet_scan_code);
+    }
+
+    private void initClick(){
+        scanLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
